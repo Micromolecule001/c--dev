3d9ae755a6f0ddd7dd35a0d3a485ffc3b18887f1
Micromolecule001 <dima.boyko.bd@gmail.com>
2024-10-10 11:59:20 +0300

    add git ignore

--- Lab1/PuzzleSolver/obj/Debug/net8.0/PuzzleSolver.AssemblyInfo.cs
+++ Lab1/PuzzleSolver/obj/Debug/net8.0/PuzzleSolver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuzzleSolver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d40e6d64e2dfdd8502d69b275d9bd2368e47c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f84512563f31683d1de46d278e7c753e663978")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuzzleSolver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuzzleSolver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
